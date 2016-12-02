e3ea5b59bc42912ec26a40aef4e0753afaead337
Sunil <slysunil101@gmail.com>
2016-12-01 19:10:41 -0800

    get data from senate.gov

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"net/http"
+	"io/ioutil"
 	"log"
+	"fmt"
 )
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	//log.Println(r.URL.Path);
 	//t := template.New("template");
 	//t, _ = t.ParseFile("/views/home.html")
-	
+
 	http.ServeFile(w, r, "." + r.URL.Path);
 }
 
 func serveStateInfo(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://www.senate.gov/general/contact_information/senators_cfm.xml");
+
+	if (err != nil) {
+		log.Println("Error getting info!");
+		return;
+	}
+	defer resp.Body.Close();
+
+	body, err := ioutil.ReadAll(resp.Body);
+	if (err != nil) {
+		log.Println("Error reading response body!");
+		return;
+	}
+
+	fmt.Printf("%s", body);
+
 	log.Println("Got request for state info: " + r.URL.Path);
 }
 
