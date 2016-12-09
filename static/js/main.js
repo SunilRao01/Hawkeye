fcfdde5beac2aa8fc7321a36e40b9fba734de0e5
Sunil <slysunil101@gmail.com>
2016-12-09 10:50:09 -0800

    federal senate complete

--- static/js/main.js
+++ static/js/main.js
@@ -1,44 +1,44 @@
-var cardTemplate = `
-                      <h4 class="card-name">[[NUM]]. John Doe</h4>
-                      <h6 class="card-state">IL</h6>
-                      <center><img class="u-max-full-width card-image" src="/static/images/sample-person.jpg"></center>
-                      <div class="container">
-                        <div class="row">
-                          <p class="one-half column card-label">Party:</p>
-                          <p class="one-half column card-description">[[PARTY]]</p>
-                        </div>
-                        <hr>
-                        <div class="row">
-                          <p class="one-half column card-label">Website:</p>
-                          <p class="one-half column card-description">[[WEBSITE]]</p>
-                        </div>
-                        <hr>
-                        <div class="row">
-                          <p class="one-half column card-label">Contact:</p>
-                          <p class="one-half column card-description">[[CONTACT]]</p>
-                        </div>
-                      </div>
-                  `;
+var federalSenatePartial = 
+`
+  <h4 class="card-name">[[FIRSTNAME]] [[LASTNAME]]</h4>
+  <h6 class="card-state">[[STATE]]</h6>
+  <center><img class="u-max-full-width card-image" src="/static/images/sample-person.jpg"></center>
+  <div class="container">
+    <div class="row">
+      <p class="one-half column card-label">Party:</p>
+      <p class="one-half column card-description">[[PARTY]]</p>
+    </div>
+    <hr>
+    <div class="row">
+      <p class="one-half column card-label">Website:</p>
+      <a href="[[WEBSITE]]" target="_blank" class="one-half column card-description">[[WEBSITE]]</a>
+    </div>
+    <hr>
+    <div class="row">
+      <p class="one-half column card-label">Contact:</p>
+      <a href="[[CONTACT]]" class="one-half column card-description">[[CONTACT]]</p>
+    </div>
+  </div>
+`;
+
 var currentRow = document.createElement('div');
+var container;
 function populateUI()
 {
-	// NOTE: Currenly populating UI info with dummy data for testing purposes,
-	// later on, use the information from the JSON responses
+	container = document.getElementById('cardContainer');
 	
 
-
-	// NOTE: For now, populate it with 6 dummies for UI testing
+	// Populate federal senate info
+	cardIndex = 0;
 	var newCard = document.createElement('div');
-	newCard.innerHTML = cardTemplate;
+	newCard.innerHTML = federalSenatePartial;
 
 	
 	currentRow.className = 'row';
 	var i = 0;
-	var tempCardCount = 4;
-	for (i = 0; i < tempCardCount; i++) {
+	for (i = 0; i < jsonSenate.length; i++) {
 		if (currentRow.childElementCount == 3) {
-			console.log('making new row! @ step: ' + i);
-			document.getElementById('cardContainer').appendChild(currentRow);
+			container.appendChild(currentRow);
 			
 
 			currentRow = document.createElement('div');
@@ -46,52 +46,68 @@ function populateUI()
 			
 			currentRow.appendChild(getAnotherCard());
 			
-			console.log('new row count: ' + currentRow.childElementCount)
-		} else if (i == tempCardCount-1) {
-			console.log('last element reached?' + i);
+		} else if (i == jsonSenate.length-1) {
 
 			var currentCard = document.createElement('div');
-			currentCard.className = "one-third column card-D";
+			currentCard.className = "one-half column";
+			
 			
 
 			currentRow.appendChild(getAnotherCard());
 
-			document.getElementById('cardContainer').appendChild(currentRow);
+			container.appendChild(currentRow);
 		} else {
-			console.log("step: " + i);
 			// TODO: Would actually get the card ready, not dummies like now
 			currentRow.appendChild(getAnotherCard());
 		}
 	}
-	
-	document.getElementById('cardContainer').appendChild(currentRow);
-	console.log('finished... Card Count: ' + tempCardCount);
-	
-
-	
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+	// Add last row of federal senators
+	container.appendChild(currentRow);	
 }
 
-var counter = 0;
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
 
+var jsonSenate = {};
+var counter = 0;
+var cardIndex = 0;
 function getAnotherCard()
 {
 	var currentCard = document.createElement('div');
-	currentCard.className = "one-third column card-D";
-			
+	currentCard.className = "one-half column";
+	if (jsonSenate[cardIndex].Party == "R") {
+		currentCard.className += " card-R ";
+	} else if (jsonSenate[cardIndex].Party == "D") {
+		currentCard.className += " card-D ";
+	}
 
-	assignCardTemplate(counter);
+	var currentTemplate = federalSenatePartial.slice();
+	 currentTemplate = currentTemplate.replace("[[FIRSTNAME]]", jsonSenate[cardIndex].First_name.toString());
+	currentTemplate = currentTemplate.replace("[[LASTNAME]]", jsonSenate[cardIndex].Last_name.toString());
+	currentTemplate = currentTemplate.replace("[[STATE]]", jsonSenate[cardIndex].State.toString());
+	currentTemplate = currentTemplate.replace("[[PARTY]]", jsonSenate[cardIndex].Party.toString());
+	currentTemplate = currentTemplate.replace("[[WEBSITE]]", jsonSenate[cardIndex].Website.toString());
+	currentTemplate = currentTemplate.replace("[[WEBSITE]]", jsonSenate[cardIndex].Website.toString());
+	currentTemplate = currentTemplate.replace("[[CONTACT]]", jsonSenate[cardIndex].Email.toString());
+	currentTemplate = currentTemplate.replace("[[CONTACT]]", jsonSenate[cardIndex].Email.toString());
+
+	if (cardIndex < jsonSenate.length-1) {
+		cardIndex++;
+	} else {
+		cardIndex = 0;
+	}
+
 	counter++;
-	currentCard.innerHTML = cardTemplate;
+	currentCard.innerHTML = currentTemplate;
+
 	return currentCard;
 }
 
-function assignCardTemplate(inputVar)
-{
-	cardTemplate = cardTemplate.replace("[[NUM]]", inputVar);
-	cardTemplate = cardTemplate.replace("[[PARTY]]", "N/A");
-	cardTemplate = cardTemplate.replace("[[WEBSITE]]", "N/A");
-	cardTemplate = cardTemplate.replace("[[CONTACT]]", "N/A");
-}
+
 
 function getResults() {
 	// Retrite selected option
@@ -117,14 +133,16 @@ function httpGetAsync(theUrl, callback) {
     xmlHttp.send(null);
 }
 
+
+
 function setSenateInfo(senateInfo) {
-	console.log("senate info response: " + senateInfo);
-	console.log("Settings senate info...");
+	jsonSenate = JSON.parse(senateInfo);
+
 
 	populateUI();
 }
 
 function setRepsInfo(repsInfo) {
-	console.log("representatives info response: " + repsInfo);
-	console.log("Settings HouseOfReps info...");
+	//console.log("representatives info response: " + repsInfo);
+	//console.log("Settings HouseOfReps info...");
 }
